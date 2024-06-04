ce836f693c3c0e680cf9163f77cae79ea708e4cf
Tyler O'Neil <tyleroneil523@gmail.com>
2024-06-04 09:25:54 -0800

    Added Function parameters

--- SwiftSnowflakeDetector/SwiftSnowflakeDetector/main.swift
+++ SwiftSnowflakeDetector/SwiftSnowflakeDetector/main.swift
@@ -9,19 +9,20 @@ import Foundation
 
 // Use an array as input
 // Array of 8 Ints
-let snowflake = [1,2,3,4,5,1,6,7,]
+let snowflake = [1,2,3,4,5,6,7]
+var loopCount = 0
 
 var seen = Set<Int>()
 
 // Using the Set Strucuture to track seen integers
-func identifySame() {
-    for flake in snowflake {
-        if seen.contains(flake) {
-            print("Duplicate found \(flake)")
+func identifySame(values: Array<Int>, n: Int) {
+    for each in values {
+        if values[each] == n {
+            print("Match Found")
             break
         }
-        seen.insert(flake)
+        seen.insert(each)
     }
 }
 
-identifySame()
+identifySame(values: snowflake, n: 5)
